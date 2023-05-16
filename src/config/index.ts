cb0fc16903cb70cedc74dc2bcf1a761620b06a7b
Leandro Corbalan <leandrocorbalan88@gmail.com>
2023-05-15 23:56:27 -0300

    database connection

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,13 +6,29 @@ export interface IAppConfig {
     env: string;
     port: number;
   };
+  db: {
+    url: string;
+    name: string;
+    params: string;
+    uri: string;
+  };
 }
 
 export default (): IAppConfig => {
+  const dbUrl = demandEnv('DB_URL'),
+    dbName = demandEnv('DB_NAME'),
+    dbParams = demandEnv('DB_PARAMS');
+
   return {
     app: {
       env: demandEnv('ENV', ENVIRONMENT.DEV),
       port: parseInt(demandEnv('PORT', DEFAULT_APP_PORT), 10),
+    },
+    db: {
+      url: dbUrl,
+      name: dbName,
+      params: dbParams,
+      uri: `${dbUrl}/${dbName}?${dbParams}`,
     },
   };
 };
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import config from '../../config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [config] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('db.uri'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}

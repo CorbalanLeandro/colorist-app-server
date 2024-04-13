af223eb7e242bacc1cdba066f8e01549bf175c70
Leandro Corbalan <leandrocorbalan88@gmail.com>
2024-04-13 18:16:00 -0300

    CA-15 | custom logger

---
+++ src/logger/index.ts
@@ -0,0 +1,45 @@
+import { ConsoleLogger } from '@nestjs/common';
+
+interface ILogObjectWithError {
+  err: any;
+  error: any;
+  [key: string]: any;
+}
+
+export class CustomLogger extends ConsoleLogger {
+  error(message: any, ...optionalParams: [...any]): void {
+    try {
+      const logObjectWithErrorIndex = optionalParams.findIndex(
+        (p) => p?.hasOwnProperty('err') || p?.hasOwnProperty('error'),
+      );
+
+      const logObjectWithError: ILogObjectWithError | undefined =
+        logObjectWithErrorIndex !== -1
+          ? optionalParams.splice(logObjectWithErrorIndex, 1)[0]
+          : undefined;
+
+      if (logObjectWithError) {
+        const { err: pureErr, error: pureError } = logObjectWithError;
+
+        const err = pureErr ? pureErr.stack || pureErr.toString() : undefined;
+
+        const error = pureError
+          ? pureError.stack || pureError.toString()
+          : undefined;
+
+        logObjectWithError.err = err;
+        logObjectWithError.error = error;
+
+        // log object must be first
+        optionalParams.unshift(logObjectWithError);
+      }
+    } catch (err) {
+      console.error(
+        { err, optionalParams },
+        'Cannot serialize the error object on the log',
+      );
+    }
+
+    super.error(message, ...optionalParams);
+  }
+}
